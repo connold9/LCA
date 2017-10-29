04f52ea726453c2994ab1ef33c68108a17217db2
Devin Connolly <32296346+connold9@users.noreply.github.com>
2017-10-29 17:53:18 +0000

    Added Size, isEmpty, Contains
    Added some methods to the DAG class. Some are similar to the ones previously deleted but feel more confident doing it this way.

--- DAG.java
+++ DAG.java
@@ -9,5 +9,33 @@ public class DAG <Value> {
 			this.val = val;
 		}
 	}
+	
+	@SuppressWarnings("unchecked")
 	private Node[] nodeList = new DAG.Node[0];
+	
+	public boolean isEmpty() { //method to check if the DAG is empty
+		if (size()==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	
+	public int size() { 
+		return nodeList.length;  //returns number of nodes the DAG
+	}
+	
+	// Checks whether Node n is in the graph.
+	public boolean contains(Value v) {
+		boolean valInList = false;
+		for (int i=0; i<nodeList.length; i++) {
+			if (nodeList[i].val == v) {
+				valInList = true;
+				break;
+			}			
+		}
+		return valInList;
+	}
 }
